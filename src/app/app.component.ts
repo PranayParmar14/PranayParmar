7dd0825af77c17bbe0e0c7fd50dd9baf96e14454
pranayp <parmarpranay30@gmail.com>
2025-02-08 14:52:55 -0500

    dark and light modes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,35 @@ import { FooterComponent } from "./footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit{
+  constructor() {}
+  isDarkMode: boolean = false;
+
+
+  ngOnInit(): void {
+    if (this.isBrowser()) {
+      const savedTheme = localStorage.getItem('theme');
+      this.isDarkMode = savedTheme === 'dark';
+      this.applyTheme();
+    }
+  }
+
+    toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isBrowser()) {
+      localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+      this.applyTheme();
+    }
+  }
+
+  applyTheme(): void {
+    const theme = this.isDarkMode ? 'dark-theme' : 'light-theme';
+    document.body.className = theme; // Apply the theme class to the <body> tag
+  }
+
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,23 +6,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent implements OnInit {
-  constructor() {}
-  isDarkMode: boolean = false;
+export class NavbarComponent  {
 
-
-  ngOnInit() {const savedTheme = localStorage.getItem('theme');
-    this.isDarkMode = savedTheme === 'dark';
-    this.applyTheme();
-}
-
-toggleTheme(): void {
-  this.isDarkMode = !this.isDarkMode;
-  localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
-  this.applyTheme();
-}
-applyTheme(): void {
-  const theme = this.isDarkMode ? 'dark-theme' : 'light-theme';
-  document.body.className = theme; // Apply the theme class to the <body> tag
-}
 }
