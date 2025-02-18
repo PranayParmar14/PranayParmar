754bd9c0c0171a7577e229e8b303e37a0000d7e4
pranayp <parmarpranay30@gmail.com>
2025-02-17 22:09:20 -0500

    timeline and styles

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,55 +1,56 @@
 import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { NgbModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
-  imports: [CommonModule],
+  imports: [NgbModule, CommonModule],
 })
 export class ExperienceComponent {
-  timelineData = [
-    {
-      date: 'Feb 14, 2023',
-      title: "Valentine's Day",
-      details: 'A day to celebrate love and affection.',
-    },
-    {
-      date: 'Sep 7, 2023',
-      title: 'Project Launch',
-      details: 'Official launch of our new project.',
-    },
-    {
-      date: 'Mar 7, 2023',
-      title: 'Team Meetup',
-      details: 'Annual team meetup and strategy planning.',
-    },
-  ];
-
-  isMobile = false; // Default to false
-  activePoint: number | null = null;
-  hoveredPoint: number | null = null;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    if (isPlatformBrowser(this.platformId)) {
-      this.isMobile = window.innerWidth <= 768; // Only runs in the browser
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.isMobile = window.innerWidth <= 768;
-    }
-  }
-
-  openModal(index: number) {
-    this.activePoint = this.activePoint === index ? null : index; // Toggle modal
-    Modal.open();
-  }
-
-  closeModal() {
-    this.activePoint = null;
-  }
+  //TIMELINE STUFF
+  // events = [
+  //   {
+  //     date: 'Feb 14, 2023',
+  //     title: "Valentine's Day",
+  //     details: 'A day to celebrate love and affection.',
+  //   },
+  //   {
+  //     date: 'Sep 7, 2023',
+  //     title: 'Project Launch',
+  //     details: 'Official launch of our new project.',
+  //   },
+  //   {
+  //     date: 'Mar 7, 2023',
+  //     title: 'Team Meetup',
+  //     details: 'Annual team meetup and strategy planning.',
+  //   },
+  // ];
+  // isMobile = false; // Default to false
+  // activePoint: number | null = null;
+  // hoveredPoint: number | null = null;
+  // constructor(
+  //   @Inject(PLATFORM_ID) private platformId: Object,
+  //   private modalService: NgbModal
+  // ) {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     this.isMobile = window.innerWidth <= 768; // Only runs in the browser
+  //   }
+  // }
+  // @HostListener('window:resize', ['$event'])
+  // onResize() {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     this.isMobile = window.innerWidth <= 768;
+  //   }
+  // }
+  // openModalFunction(content: any, index: number) {
+  //   this.activePoint = index; // Ensures `activePoint` is always a valid number
+  //   this.modalService.open(content);
+  // }
+  // //function to close modal
+  // closeModalFunction() {
+  //   this.modalService.dismissAll();
+  // }
 }
